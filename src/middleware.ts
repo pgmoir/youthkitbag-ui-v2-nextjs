c3d821d8402c8e2af0021231f770d9e8718f5a9b
Philip Moir <phil@captur.ai>
2025-05-02 11:05:55 +0100

    Log the request object so that it is used - ESLINT issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,6 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export default function middleware(request: NextRequest) {
+  console.log(request.url);
   return NextResponse.next();
 }
